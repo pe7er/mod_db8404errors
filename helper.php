b73d8a76f2720998013c8f4a443b75d5c79617cb
Peter Martin <pe7er@db8HQ>
2015-01-27 01:43:57 +0100

    first version

---
+++ helper.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package     mod_db8_404errors
+ * @author      Peter Martin, www.db8.nl
+ * @copyright   Copyright (C) 2015 Peter Martin. All rights reserved.
+ * @license     GNU General Public License version 2 or later.
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Helper for mod_db8_404errors
+ */
+abstract class ModDb8404errorsHelper
+{
+    /**
+     * Gets a list of 404 Errors from Redirect Manager
+     */
+    public static function getItems(&$params)
+    {
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
+        $query->select('a.id, a.old_url, a.new_url, a.referer, a.hits, a.published, a.created_date');
+        $query->from('#__redirect_links AS a');
+        if ($params->get('show') < 3) {
+            $query->where('a.published = ' . $params->get('show'));
+        }
+        $query->order($db->escape($params->get('ordering', 'a.old_url')) . ' ' . $db->escape($params->get('direction', 'ASC')));
+        $db->setQuery($query, 0, intval($params->get('count', 5)));
+        $list = $db->loadObjectList();
+
+        return $list;
+    }
+}
+
---
+++ tmpl/default.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @package     mod_db8_404errors
+ * @author      Peter Martin, www.db8.nl
+ * @copyright   Copyright (C) 2015 Peter Martin. All rights reserved.
+ * @license     GNU General Public License version 2 or later.
+ */
+
+defined('_JEXEC') or die;
+
+JHtml::_('bootstrap.tooltip');
+?>
+<div class="row-striped">
+	<?php if (count($list)) : ?>
+		<?php foreach ($list as $i => $item) : ?>
+			<?php $item->link = JRoute::_('index.php?option=com_redirect&task=link.edit&id=' . $item->id); ?>
+
+			<?php $hits = (int) $item->hits; ?>
+			<?php $hits_class = ($hits >= 25 ? 'important' : ($hits >= 10 ? 'warning' : ($hits >= 5 ? 'info' : ''))); ?>
+			<div class="row-fluid">
+				<div class="span9">
+					<span class="badge badge-<?php echo $hits_class; ?> hasTooltip" title="<?php echo JHtml::tooltipText('JGLOBAL_HITS'); ?>"><?php echo $item->hits; ?></span>
+					<strong class="row-title break-word">
+						<?php if ($item->link) : ?>
+							<a href="<?php echo $item->link; ?>">
+								<?php echo str_replace(JUri::root(), '', rawurldecode($item->old_url)); ?>
+							</a>
+						<?php else : ?>
+							<?php echo str_replace(JUri::root(), '', rawurldecode($item->old_url)); ?>
+						<?php endif; ?>
+					</strong>
+				</div>
+				<div class="span3">
+					<span class="small">
+						<i class="icon-calendar"></i>
+						<?php echo JHtml::_('date', $item->created_date, JText::_('DATE_FORMAT_LC4')); ?>
+					</span>
+				</div>
+			</div>
+		<?php endforeach; ?>
+	<?php else : ?>
+		<div class="row-fluid">
+			<div class="span12">
+				<div class="alert"><?php echo JText::_('MOD_DB8_404ERRORS_NO_MATCHING_RESULTS'); ?></div>
+			</div>
+		</div>
+	<?php endif; ?>
+</div>
